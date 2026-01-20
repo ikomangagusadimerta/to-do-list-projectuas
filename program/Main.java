d25757702e95239e52be05663aa9459928e18545
ibgputragautama-code <ibgputragautama>
2026-01-20 23:51:59 +0800

    Menambahkan deskripsi pada Main.java

--- program/Main.java
+++ program/Main.java
@@ -4,7 +4,9 @@ import ui.ToDoListApp;
 
 public class Main {
     public static void main(String[] args) {
+         // Menjalankan GUI di Event Dispatch Thread (EDT)
         javax.swing.SwingUtilities.invokeLater(() -> {
+            // Membuat instance aplikasi To-Do List
             new ToDoListApp().setVisible(true);
         });
     }
